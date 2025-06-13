db024fe2cef3534d6dfbdc71f681a970d2405cff
Sonia-hhl <103532392+Sonia-hhl@users.noreply.github.com>
2025-06-13 14:32:14 +0330

    Update wsgi.py

--- bookingsite/wsgi.py
+++ bookingsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookingsit project.
+WSGI config for bookingsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookingsit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookingsite.settings')
 
 application = get_wsgi_application()
